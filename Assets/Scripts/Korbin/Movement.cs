aa88b08112734b0bb3c079ffee8040ea0d97a6d4
Korbin <klnevius@gmail.com>
2023-12-03 14:47:13 -0500

    added collision to temp map and working on visisibility script

---
+++ Assets/Scripts/Korbin/Movement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float speed = 18f;
+
+    private Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+        float zAxis = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(hAxis,0,zAxis) * (speed * Time.deltaTime);
+        rb.MovePosition(transform.position + movement);
+    }
+}
