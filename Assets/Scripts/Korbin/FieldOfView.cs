fc87dcb0a06e34e83db47481fc48a9b4b9bf9c4d
Korbin <klnevius@gmail.com>
2023-12-03 22:42:27 -0500

    Made visibility layer shader and fov work. Needs adjustment

--- Assets/Scripts/Korbin/FieldOfView.cs
+++ Assets/Scripts/Korbin/FieldOfView.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class FieldOfView : MonoBehaviour
 {
+	//Credit to Sebastian Lague for the GitHub source code 
+	//https://github.com/SebLague/Field-of-View/blob/master/Episode%2002/Scripts/FieldOfView.cs
+	//and YouTube video series to set it up and create the scripts
+	//https://www.youtube.com/watch?v=rQG9aUWarwE (1)
+	//https://www.youtube.com/watch?v=73Dc5JTCmKI (2)
+	//https://www.youtube.com/watch?v=xkcCWqifT9M (3)
+	
     public float viewRadius;
 	[Range(0,360)]
 	public float viewAngle;
