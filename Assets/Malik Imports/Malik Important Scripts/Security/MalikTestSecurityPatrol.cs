29b30bb015e9648f3b1b83d302f91a8d1fb2cea3
Likam07 <georgemalik29@gmail.com>
2023-12-04 20:17:04 -0500

    Added the securirty movement scene

--- Assets/Malik Imports/Malik Important Scripts/Security/MalikTestSecurityPatrol.cs
+++ Assets/Malik Imports/Malik Important Scripts/Security/MalikTestSecurityPatrol.cs
@@ -4,7 +4,6 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Events;
-
 public class MalikTestSecurityPatrol : MonoBehaviour
 {
     public enum PatrolState
@@ -12,6 +11,12 @@ public class MalikTestSecurityPatrol : MonoBehaviour
         None,
         ChasePlayer,
         Patrol
+    }
+
+    public enum WaypointBehaviour
+    {
+        PingPong,
+        Loop
     }
 
     private GameObject player;
@@ -23,35 +28,33 @@ public class MalikTestSecurityPatrol : MonoBehaviour
 
     // Security Patrol
     private Vector3 destPoint; // The destination that the security guard is walking to
-    private bool walkpointSet; // If the security guard is walking to a set point or not
-    [SerializeField] private float patrolRange; // How for the security guard is allowed to walk 
+    // private bool walkpointSet; // If the security guard is walking to a set point or not
+  // [SerializeField] private float patrolRange; // How far the security guard is allowed to walk 
 
     //state change
-    [SerializeField] private float sightRange;
-    [SerializeField] private float _stoppingDistance;
+    [SerializeField] private float maxSightRange;
 
     [Header("State")] public PatrolState patrolState;
-
-
-
-    //New stuff
+    private PatrolState previousState;
+    
     public List<GameObject> _waypoints;
     [SerializeField] float speed = 2;
-    int index = 0;
+    int _waypointIndex = 0;
+    private int _waypointDirection = 1;
+    [SerializeField] private WaypointBehaviour _waypointBehaviour;
+    private float _giveUpChaseTimer;
     
+    [Tooltip("Time in seconds where we cannot see the player while chasing to stop chasing.")]
+    [SerializeField] private float GiveUpChaseTime; 
     
     //Trying Unity Events
 
     public UnityEvent BeganChase;
-
     public UnityEvent GoBackToPatrol;
-
     public GameObject firstSphere;
-    
-    
 
-   // [SerializeField] private bool chase;
-
+    // [SerializeField] private bool chase;
+    private RaycastHit hit;
     // Start is called before the first frame update
     void Start()
     {
@@ -64,85 +67,73 @@ public class MalikTestSecurityPatrol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Physics.CheckSphere(transform.position, sightRange, playerLayer))
-        { 
-            
-            
-            RaycastHit hit = new RaycastHit();
-            if (Physics.Raycast(transform.position, player.transform.position - transform.position, out hit,
-                    sightRange))
+            if (Physics.Raycast(transform.position, player.transform.position - transform.position, out hit, maxSightRange))
             {
-                Debug.Log("I am updating the Raycast");
                 if (hit.collider.gameObject == player) //&& chase )
                 {
-                    Debug.Log("I am stuck chasing the player");
                     //I see you!
                     patrolState = PatrolState.ChasePlayer;
-                    Debug.Log("I see you!");
                     //chase = false;
                 }
-                if (hit.collider.gameObject != player) //&& chase == false )
+
+                _giveUpChaseTimer = 0;
+            }
+            else
+            {
+                //if cant see the player... (and are in the chase player state)
+                if (patrolState == PatrolState.ChasePlayer)
                 {
-                    Debug.Log("I want to chase the player but I am told to patrol");
-                    //i see a wall? keep patrolling
-                    patrolState = PatrolState.Patrol;
-                    this.gameObject.transform.position = firstSphere.transform.position;
-                    // chase = true;
+                    _giveUpChaseTimer += Time.deltaTime;
+
+                    //Stop chasing the player.
+                    if (_giveUpChaseTimer > GiveUpChaseTime)
+                    {
+                        patrolState = PatrolState.Patrol;
+                        _giveUpChaseTimer = 0;
+                    }
                 }
-                
-                
-                // else
-                // {
-                //     //i see a wall? keep patrolling
-                //     patrolState = PatrolState.Patrol;
-                // }
             }
 
-        }
-        else
-        {
-            //i'm not close enough to see the player, even if it would be in line of sight
-            patrolState = PatrolState.Patrol;
-        }
-        // Checks for the player in the specified radius. If it finds anything on Player 1's layer, 
-        // in the sightRange amount around the player transform it will set playerInsight as true.
-        // If nothing is detected, it would return false.
+          
 
-        
-        // if (patrolState == PatrolState.Patrol && patrolState == PatrolState.ChasePlayer)
+            // if (patrolState == PatrolState.Patrol && patrolState == PatrolState.ChasePlayer)
         // {
         //     // If player 1 is not in sight, continue to patrol.
         //     PatrolTick();
         // }
         if (patrolState == PatrolState.ChasePlayer)
         {
-            // If player 1 is in sight, game over!!!
-            BustedTick();
-            
-            
-            BeganChase?.Invoke();
-            
-            
-            //I tried the coroutine below
-            //ChasePlayerThenStop();
-        }
-        if (patrolState == PatrolState.Patrol)
+            if(previousState != PatrolState.ChasePlayer)
+            {
+                Debug.Log("start chase player");
+                //first frame chasing player.
+                BeganChase?.Invoke();
+            }
+            //
+            ChasePlayerTick();
+        }else if (patrolState == PatrolState.Patrol)
         {
+            if (previousState != PatrolState.Patrol)
+            {
+                Debug.Log("Start Patrol");
+                //get closest spot on waypoint path....
+                GoBackToPatrol?.Invoke();
+            }
+
             // If player 1 is not in sight, continue to patrol.
             PatrolTick();
-            
-            GoBackToPatrol?.Invoke();
-
         }
         else
         {
             PatrolTick();
-            Debug.Log("No patrol state?");
+            Debug.Log("No state?");
         }
+
+        previousState = patrolState;
     }
 
     // When the security guard locates and walks toward the player
-    public void BustedTick()
+    public void ChasePlayerTick()
     {
         agent.SetDestination(player.transform.position);
     }
@@ -151,35 +142,45 @@ public class MalikTestSecurityPatrol : MonoBehaviour
     public void  PatrolTick()
     {
         //when we are close enough to the target destination, find a new random target destination.
+    
+        // Enables the security guards to move toward the gameobjects in the _waypoints list. Once it reaches the last
+        // gameobject in the list, the security guard will go back to the position of the first gameobject
+        //  in the list and begin the pattern again.
         
-            // Enables the security guards to move toward the gameobjects in the _waypoints list. Once it reaches the last
-            // gameobject in the list, the security guard will go back to the position of the first gameobject
-            //  in the list and begin the pattern again.
-        
-            
-            Vector3 destination = _waypoints[index].transform.position;
-            Vector3 newPos = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
-            transform.position = newPos;
+        Vector3 destination = _waypoints[_waypointIndex].transform.position;
+        Vector3 newPos = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
+        transform.position = newPos;
 
-            float distance = Vector3.Distance(transform.position, destination);
-            if (distance <= 1)
+        float distance = Vector3.Distance(transform.position, destination);
+        if (distance <= 1)
+        {
+            GoToNextWaypoint();
+        }
+        
+        agent.SetDestination(_waypoints[_waypointIndex].transform.position);
+
+    }
+
+    private void GoToNextWaypoint()
+    {
+        if (_waypointBehaviour == WaypointBehaviour.PingPong)
+        {
+            if (_waypointIndex + _waypointDirection >= _waypoints.Count || _waypointIndex + _waypointDirection < 0)
             {
-                if (index < _waypoints.Count - 1)
-                {
-                    index++;
-                }
-
-                else
-                {
-                    if (index >= _waypoints.Count - 1)
-                    {
-                        index = 0;
-                    }
-
-                }
-
+                //Flip the direction
+                _waypointDirection = _waypointDirection * -1;
             }
 
+            _waypointIndex = _waypointIndex + _waypointDirection;
+        }else if (_waypointBehaviour == WaypointBehaviour.Loop)
+        {
+            _waypointIndex += 1;
+
+            if (_waypointIndex >= _waypoints.Count)
+            {
+                _waypointIndex = 0;
+            }
+        }
     }
 
     // public void ChasePlayerThenStop()
