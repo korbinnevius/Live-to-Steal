1f3d197c5317bcdb46c7fe2b8a2833cbf6bff1d5
Likam07 <georgemalik29@gmail.com>
2023-12-10 18:10:46 -0500

    new input system

--- Assets/Malik Imports/Malik Important Scripts/Player/PlayerMovement.cs
+++ Assets/Malik Imports/Malik Important Scripts/Player/PlayerMovement.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using TiltFive;
 using UnityEngine;
-using Input = UnityEngine.Input;
-
-//using Input = TiltFive.Input;
+//using Input = UnityEngine.Input;
+using Input = TiltFive.Input;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float movementSpeed = 5;
+    public float movementSpeed = 1;
     public GameObject _player;
     public bool canMove;
     private Rigidbody _rigidbody;
@@ -25,27 +24,27 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // // When the player moves the joystick, their character will walk in that direction times the movement speed and 
-        // // time.
-        //
-        // if (canMove && TiltFive.Input.TryGetStickTilt(out Vector2 joystick, TiltFive.ControllerIndex.Right,
-        //         TiltFive.PlayerIndex.One))
-        // {
-        //     _player.transform.Translate(joystick.x * Time.deltaTime * movementSpeed, 0.0f,
-        //         joystick.y * Time.deltaTime * movementSpeed);
-        //     
-        //     
-        // }
-
-        if (canMove)
+        // When the player moves the joystick, their character will walk in that direction times the movement speed and 
+        // time.
+        
+        if (canMove && TiltFive.Input.TryGetStickTilt(out Vector2 joystick, TiltFive.ControllerIndex.Right,
+                TiltFive.PlayerIndex.One))
         {
-            float hAxis = Input.GetAxis("Horizontal");
-            float zAxis = Input.GetAxis("Vertical");
-        
-            Vector3 movement = new Vector3(hAxis,0,zAxis) * (movementSpeed * Time.deltaTime);
-            cc.Move(movement);
+            _player.transform.Translate(joystick.x * Time.deltaTime * movementSpeed, 0.0f,
+                joystick.y * Time.deltaTime * movementSpeed);
+            
+            
         }
-        
+
+        // if (canMove)
+        // {
+        //     float hAxis = Input.GetAxis("Horizontal");
+        //     float zAxis = Input.GetAxis("Vertical");
+        //
+        //     Vector3 movement = new Vector3(hAxis,0,zAxis) * (movementSpeed * Time.deltaTime);
+        //     cc.Move(movement);
+        // }
+        //
         
     }
     
