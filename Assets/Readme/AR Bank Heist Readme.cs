96e8db1eff67f932efb1be6ab2b23d210c55b552
Likam07 <georgemalik29@gmail.com>
2023-12-13 16:15:53 -0500

    Added A readme and edit a script

---
+++ Assets/Readme/AR Bank Heist Readme.cs
@@ -0,0 +1,33 @@
+
+//AR Bank Heist
+
+// AR Bank Heist is a two player experience, in which one player is a burglar trying to rob a bank in the dark and avoid security guards while the other player is
+// a hacker on a laptop guiding the burglar through the bank using the camera feeds and a physical printed blueprint of the bank.
+
+// Player 1 Input
+
+// Use the joystick on the Tilt Five Wand to navigate the scene.
+// Press 1 to enable the camera
+// Press A to restart the experience
+
+// Player 2 Input
+
+// Player 2 has no physical input
+
+// Sources
+
+// Siren:
+
+//SoundEffectsFactory. (2012). Ultimate Alarm Sound Effect (Gaming Sound Effects). 
+   // Retrieved from YouTube: https://www.youtube.com/watch?v=S_aZw7Rr8h4&list=PLFC4160D664618993&index=3
+
+//Background Music:
+
+//Besss, A. (n.d.). Psycho. Retrieved from Music Vine: https://musicvine.com/track/alex-besss/psycho
+
+//Font:
+
+//Fonts, I. (2022, 03 04). Normandy Beach Font. Retrieved from Font Space: https://www.fontspace.com/normandy-beach-font-f75694
+
+
+
