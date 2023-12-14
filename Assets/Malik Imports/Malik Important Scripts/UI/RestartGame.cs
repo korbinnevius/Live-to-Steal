07bc07ed137add9054d6a9b365ecba3a82d4cb54
Korbin <klnevius@gmail.com>
2023-12-13 20:28:37 -0500

    Changing mats, addings lights, restart with comp

--- Assets/Malik Imports/Malik Important Scripts/UI/RestartGame.cs
+++ Assets/Malik Imports/Malik Important Scripts/UI/RestartGame.cs
@@ -10,7 +10,7 @@ public class RestartGame : MonoBehaviour
     public TiltFive.Input.WandButton interactButton;
     void Update()
      {
-         if (TiltFive.Input.GetButtonDown(interactButton))
+         if (TiltFive.Input.GetButtonDown(interactButton) || Input.GetKeyDown(KeyCode.R))
          {
              SceneManager.LoadScene(SceneManager.GetActiveScene().name);
          }
